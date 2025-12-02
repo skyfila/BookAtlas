1bc09109036762aeead52a5885c09ab1ae5ae91e
K. Jordan <95835376+skyfila@users.noreply.github.com>
2025-12-01 20:50:52 -0500

    Delete js directory

--- js/genre_dict.js
+++
@@ -1,6 +0,0 @@
-NYT_BESTSELLERS_URL = "https://www.nytimes.com/books/best-sellers/"
-
-const parser = new DOMParser();
-const parsed = parser.parseFromString(NYT_BESTSELLERS_URL, "text/html");
-
-console.log(parsed.firstChild.innerText);
